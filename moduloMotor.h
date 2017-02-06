7014b3019e3d9e0a26c6af8f8833e1f4c6dbc5cb
ablasc <ablasc01@ucm.es>
2017-02-06 21:12:52 +0100

    Add files via upload
    Ficheros que contendrán las funciones básicas a realizar por los servomotores

---
+++ moduloMotor.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <wiringPi.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+void parar();
+
+/*****Calcular las cifras*****/
+void retroceder();
+
+void avanza();
+
+void avanzaLento();
+
+void rotaIzq();
+
+void rotaDcha();
